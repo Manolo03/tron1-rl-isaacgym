1d53df803e23dd0c9a2063bd9fcc8158b15b5f5f
Manolo03 <lacournathan@gmail.com>
2025-11-05 13:57:21 +0100

    Increase tracking lin vel weight from 0.5 (great run Nov05_11-12-03_)

--- legged_gym/envs/wheelfoot_stand/wheelfoot_stand_config.py
+++ legged_gym/envs/wheelfoot_stand/wheelfoot_stand_config.py
@@ -277,7 +277,7 @@ class BipedCfgWF(BaseConfig):
             keep_balance = 5.0
 
             # tracking related rewards
-            tracking_lin_vel = 0.5
+            tracking_lin_vel = 1.0 #
             tracking_ang_vel = 1.0
             base_height = 5
             # tracking_lin_vel_pb = 1.0
